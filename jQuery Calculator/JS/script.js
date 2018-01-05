934365f7d8e0ecf27a32131b9b31d6f5b6890e3f
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-01-05 17:21:45 -0600

    added the JS file

---
+++ jQuery Calculator/JS/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+  //Stores the inputs from user to a variable
+  var inputs = [""];
+  //variable to store total
+  var totalString;
+  //Operators array for validation .
+  var operators1 = ["+", "-", "/", "*"];
+  //validate the ".""
+  var operators2 = ["."];
+  //Numbers array for validation
+  var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  //Adds value to inputs array and checks validation
+  function getValue(input) {
+    if (
+      operators2.includes(inputs[inputs.length - 1]) === true &&
+      input === "."
+    ) {
+      console.log("Duplicate '.'");
+    } else if (inputs.length === 1 && operators1.includes(input) === false) {
+      //Validation to make sure it doesn't start with an operator
+      inputs.push(input);
+    } else if (operators1.includes(inputs[inputs.length - 1]) === false) {
+      //If last character was not an operator add operator to the array
+      inputs.push(input);
+    } else if (nums.includes(Number(input))) {
+      inputs.push(input);
+    }
+    update();
+  }
+  function update() {
+    totalString = inputs.join("");
+    $("#steps").html(totalString);
+    console.log(inputs);
+  }
+  function getTotal() {
+    totalString = inputs.join("");
+    $("#steps").html(eval(totalString));
+  }
+
+  $("a").on("click", function() {
+    if (this.id === "deleteAll") {
+      inputs = [""];
+      update();
+    } else if (this.id === "backOne") {
+      inputs.pop();
+      update();
+    } else if (this.id === "total") {
+      getTotal();
+    } else {
+      //error1
+      if (inputs[inputs.length - 1].indexOf("+", "-", "/", "*", ".") === -1) {
+        getValue(this.id);
+      } else {
+        getValue(this.id);
+      }
+    }
+  });
+});
